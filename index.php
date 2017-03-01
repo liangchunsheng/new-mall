c144202d0c59c181aa3458b5015f1f75bd307942
sheng <13631245388@163.com>
2017-03-01 12:53:44 +0800

    your message

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
+
 define('SITE_URL','http://www.mall.com/');
 define('SITE2_URL','http://www.mall.com/');
 define('CSS_URL','http://www.mall.com/Mall/Public/css/');
---
+++ phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+phpinfo();
+
+// $redis = new redis();
+// $redis->connect('127.0.0.1', 6379);
+
+// $redis->set('hua','qiuhua');
+// $redis->set('wen','wenjun');
+
+// echo $redis->get('hua');
